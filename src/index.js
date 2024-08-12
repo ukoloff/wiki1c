fddf1245ba84850fc90458d7bee77c3ec1910050
Stas Ukolov <ukoloff@gmail.com>
2024-08-12 15:43:43 +0500

    Render markdown

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+const md = require('markdown-it')
+const mdc = require('markdown-it-container')
 const sql = require('./sql')
 
 main()
@@ -5,5 +7,13 @@ main()
 async function main() {
   const q = await sql()
   var r = await q.query`Select Top 1 _Fld9228 as md From _Reference9221 Where _Description Like '%ssd'`
-  console.log(r)
+  await q.close()
+
+  var mdx = md({
+    html: true,
+    linkify: true,
+    typographer: true,
+  })
+  var html = mdx.render(r.recordset[0].md)
+  console.log(html)
 }
