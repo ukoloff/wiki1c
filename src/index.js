0157c557e89545d450395bc97434d55e430c3914
Stas Ukolov <ukoloff@gmail.com>
2024-08-12 17:03:00 +0500

    Fix local links

--- src/index.js
+++ src/index.js
@@ -29,8 +29,8 @@ async function main() {
         title Like '%ssd'
     `)
   var a = await q.request()
-  .input('pid', mssql.Binary, r.recordset[0].id)
-  .query(`
+    .input('pid', mssql.Binary, r.recordset[0].id)
+    .query(`
     with attachments as(
         select
             _IDRRef as id,
@@ -60,7 +60,31 @@ async function main() {
   })
     .use(mdc, 'info', { marker: '!' })
 
+  var
+    defI = mdx.renderer.rules.image,
+    defA = mdx.renderer.rules.link_open ||
+      function (tokens, idx, options, env, self) {
+        return self.renderToken(tokens, idx, options);
+      }
+
+  mdx.renderer.rules.image = function (tokens, idx, options, env, self) {
+    fixHref(tokens[idx], 'src')
+    return defI(tokens, idx, options, env, self);
+  }
+
+  mdx.renderer.rules.link_open = function (tokens, idx, options, env, self) {
+    fixHref(tokens[idx], 'href')
+    return defA(tokens, idx, options, env, self);
+  }
+
   var html = mdx.render(r.recordset[0].md)
   console.log(html)
   console.log(a.recordset)
+}
+
+function fixHref(token, attr) {
+  var val = token.attrGet(attr)
+  if (/^\/[^\/]+$/.test(val)) {
+    token.attrSet(attr, val.substr(1))
+  }
 }
