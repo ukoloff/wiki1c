b6e4408d7fc94dc18fb55d2a8a996ce6b1d6c319
Stas Ukolov <ukoloff@gmail.com>
2024-08-12 15:32:34 +0500

    Less data

--- test/sql.js
+++ test/sql.js
@@ -3,7 +3,7 @@ const test = require('node:test')
 test('Connect to MS SQL', async function (t) {
   const sql = await require('../src/sql')()
 
-  await sql.query`Select * from Config`
+  await sql.query`Select Top 10 * from Config`
   await sql.close()
 })
 
