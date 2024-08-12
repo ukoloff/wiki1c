3144745594fda6a2c177799665ac25e0e4fd63dd
Stas Ukolov <ukoloff@gmail.com>
2024-08-12 16:19:31 +0500

    Add pages CTE

--- src/index.js
+++ src/index.js
@@ -6,7 +6,27 @@ main()
 
 async function main() {
   const q = await sql()
-  var r = await q.query`Select Top 1 _Fld9228 as md From _Reference9221 Where _Description Like '%ssd'`
+  var r = await q.query`
+    with pages as(
+        select
+            _IDRRef as id,
+            _ParentIDRRef as up,
+            _Code as kod,
+            _Description as title,
+            DATEADD(YYYY, -2000, _Fld9226) as mdate,
+            _Fld9227RRef as space_id,
+            _Fld9306 as lat,
+            _Fld9228 as md
+        from
+            _Reference9221
+    )
+    Select
+        Top 1 md
+    From
+        pages
+    Where
+        title Like '%ssd'
+    `
   await q.close()
 
   var mdx = md({
