1510a4717609c71a32c5c6fb9aee1f1a696f0bef
Stas Ukolov <ukoloff@gmail.com>
2024-08-12 16:39:50 +0500

    Find attachments

--- src/index.js
+++ src/index.js
@@ -21,11 +21,32 @@ async function main() {
             _Reference9221
     )
     Select
-        Top 1 md
+        Top 1 id, md
     From
         pages
     Where
         title Like '%ssd'
+    `
+  var a = await q.query`
+    with attachments as(
+        select
+            _IDRRef as id,
+            _Description as basename,
+            _Fld9235RRef as page_id,
+            DATEADD(YYYY, -2000, _Fld9237) as mdate,
+            DATEADD(YYYY, -2000, _Fld9238) as cdate,
+            _Fld9245 as filepath,
+            _Fld9246 as bytes,
+            _Fld9247 as ext
+        from
+            _Reference9222
+    )
+    select
+        basename, ext, bytes, filepath
+    from
+        attachments
+    where
+        page_id = 0xa691cfe02c3a99ae4b11c1fc4e883b36
     `
   await q.close()
 
@@ -38,4 +59,5 @@ async function main() {
 
   var html = mdx.render(r.recordset[0].md)
   console.log(html)
+  console.log(a.recordset)
 }
