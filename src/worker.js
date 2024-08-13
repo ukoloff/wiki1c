f38f525406344af82a778887b776969307a224da
Stas Ukolov <ukoloff@gmail.com>
2024-08-13 16:57:48 +0500

    Start routing

--- src/worker.js
+++ src/worker.js
@@ -1,5 +1,14 @@
 const http = require('node:http')
+const home = require('./home')
 
 http.createServer((req, res) => {
-  res.end('Hello, world!')
+  var path = req.url
+  if ('/' == path) {
+    home(req, res)
+    return
+  } else {
+    $m = /^\/[\da-f]{4,}($|\/)/.exec(path)
+  }
+  res.statusCode = 404
+  res.end(`Path <${path}> not found!`)
 }).listen(5432, 'localhost')
