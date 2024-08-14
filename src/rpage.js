79069e105ade3cbd7324e4d83762a79bb4515afc
Stas Ukolov <ukoloff@gmail.com>
2024-08-14 13:43:32 +0500

    Render submenu

--- src/rpage.js
+++ src/rpage.js
@@ -1,13 +1,49 @@
 //
 // Render KB page
 //
+const mssql = require('mssql')
 const sql = require('./sql')
+const html = require('./h')
 
 module.exports = render
 
 async function render(res, page) {
   res.setHeader('Content-Type', 'text/html; charset=utf-8')
+  res.write(`<title>${page.title}</title>`)
+
+  if (page.ccount) {
+    await renderChildren(res, page)
+    if (page.md) res.write('<hr>')
+  }
 
   res.end('PAGE')
 }
 
+async function renderChildren(res, page) {
+  var h = await sql()
+
+  await new Promise(run)
+
+  function run(resolve, reject) {
+    res.write('<ul>')
+    var q = h.request()
+    q.stream = true
+    q
+      .on('row', row => {
+        res.write(`<li><a href=../${row.id.toString('hex')}/>${html(row.title)}</a>`)
+      })
+      .on('done', _ => {
+        res.write('</ul>')
+        resolve()
+      })
+      .on('error', reject)
+      .input('pid', mssql.Binary, page.id)
+      .query(`
+        with ${sql.pages}, ${sql.spaces}, ${sql.pagez}
+          select id, title
+          from pagez
+          where up = @pid
+          order by title
+        `)
+  }
+}
