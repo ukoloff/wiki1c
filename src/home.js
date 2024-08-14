2e8c06001f1d9da281de2181c87f909daf04b852
Stas Ukolov <ukoloff@gmail.com>
2024-08-14 08:49:44 +0500

    List top pages

--- src/home.js
+++ src/home.js
@@ -1,5 +1,27 @@
+const sql = require('./sql')
+
 module.exports = home
 
-function home(req, res) {
-  res.end('Hello, world!')
+async function home(req, res) {
+  res.setHeader('Content-Type', 'text/html; charset=utf-8')
+  res.write('<title>База Знаний</title><ul>')
+  var h = await sql()
+
+  var q = h.request()
+  q.stream = true
+  q
+    .on('row', row => {
+      res.write(`<li><a href=${row.id.toString('hex')}/>${row.title}</a>`)
+    })
+    .on('done', _ => {
+      res.end('</ul>')
+    })
+  q.query(`
+    with ${sql.pages}, ${sql.spaces}, ${sql.pagez}
+    select
+      id, title
+    from pagez
+    where up=0x00
+    order by title
+    `)
 }
