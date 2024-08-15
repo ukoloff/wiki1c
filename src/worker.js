3a577d69623b5593bd3f3d7348fb477d51187372
Stas Ukolov <ukoloff@gmail.com>
2024-08-15 14:44:51 +0500

    Extract route

--- src/worker.js
+++ src/worker.js
@@ -1,33 +1,5 @@
 const http = require('node:http')
-const home = require('./home')
-const oops = require('./404')
-const getpage = require('./getpage')
-const renderpage = require('./rpage')
-const renderfile = require('./rfile')
-const search = require('./search')
+const route = require('./route')
 
-http.createServer(process)
+http.createServer(route)
   .listen(5678)
-
-async function process(req, res) {
-  var path = req.url
-  if ('/' == path) {
-    home(req, res)
-    return
-  }
-  if (/^\/q\/($|\?)/.test(path))
-    return search(req, res)
-  var $m = /^\/([\da-f]{4,})($|\/)(.*)/.exec(path)
-  if ($m) {
-    if (!$m[2]) {
-      res.writeHead(301, $m[1] + '/')
-      return
-    }
-    var page = await getpage($m[1])
-    if (!page) return oops(req, res)
-    return $m[3] ?
-      renderfile(res, page, decodeURIComponent($m[3])) :
-      renderpage(res, page)
-  }
-  oops(req, res)
-}
