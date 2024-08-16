7c03fd82e6552ae57ccc922ed26caeaf3e9affa9
Stas Ukolov <ukoloff@gmail.com>
2024-08-16 11:57:13 +0500

    Add Bootstrap theme

--- src/head.js
+++ src/head.js
@@ -7,9 +7,16 @@ head.tail = tail
 
 function head(res, title) {
   res.setHeader('Content-Type', 'text/html; charset=utf-8')
-  res.write(`<!DOCTYPE html><html><head><title>${title}</title></head><body>`)
+  res.write(`
+<!DOCTYPE html><html><head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>${title}</title>
+<link href="https://cdn.jsdelivr.net/npm/bootswatch@5.3.3/dist/morph/bootstrap.min.css" rel="stylesheet">
+</head>
+<body><div class="container-fluid">
+  `.trim())
 }
 
 function tail(res) {
-  res.end(`</body></html>`)
+  res.end(`</div></body></html>`)
 }
