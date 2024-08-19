8d905465f441db1e402906e31b3cee6b9ef585bf
Stas Ukolov <ukoloff@gmail.com>
2024-08-19 15:59:29 +0500

    Jail in <div>

--- src/tree.js
+++ src/tree.js
@@ -37,10 +37,10 @@ async function tree(req, res) {
     res.write('<ul class="list-group">')
     for (var row of rows) {
       if (row.leaf) {
-        res.write(`<li class="list-group-item"><a href="../${row.id}/" target="KB">${html(row.title)}</a></li>`)
+        res.write(`<li class="list-group-item" title="${html(row.title)}"><a href="../${row.id}/" target="KB">${html(row.title)}</a></li>`)
         continue
       }
-      res.write(`<details name="${level}">\n<summary>${html(row.title)}</summary>\n`)
+      res.write(`<details name="$${level}">\n<summary title="${html(row.title)}">${html(row.title)}</summary>\n`)
       if (row.c.length) {
         res.write(`<div style="margin-left: 1em;">`)
         render(row.c, level+1)
@@ -53,7 +53,9 @@ async function tree(req, res) {
 
   head(res, 'Навигация')
 
+  res.write(`<div style="position: fixed; left: 0; top: 0; width: 21%; bottom: 0; overflow: auto; white-space: nowrap;">`)
   render(root.c)
+  res.write(`</div>`)
 
   head.tail(res)
 }
