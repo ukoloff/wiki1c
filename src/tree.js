3ec8a00ad8c3ce6937dba04bb974603b58ea6463
Stas Ukolov <ukoloff@gmail.com>
2024-08-19 15:10:20 +0500

    Build hierarchy

--- src/tree.js
+++ src/tree.js
@@ -7,7 +7,40 @@ const head = require('./head')
 
 module.exports = tree
 
-function tree(req, res) {
+async function tree(req, res) {
+  var h = await sql()
+  var r = await h.request().query(`
+      with ${sql.pages}, ${sql.spaces}, ${sql.pagez}
+      select
+          lower(convert(nvarchar, id, 2)) as id,
+          lower(convert(nvarchar, up, 2)) as up,
+          iif(md is null, 0, 1) as leaf,
+          title,
+          (
+              select
+                  count(*)
+              from
+                  pagez X
+              where
+                  X.up = Z.id
+          ) as n
+      from
+          pagez Z
+      order by
+          leaf,
+          title
+    `)
+  r = r.recordset
+  var idx = {}
+  for (var row of r) {
+    idx[row.id] = row
+    row.c = []
+  }
+  var root = {c: []}
+  for (var row of r) {
+    (idx[row.up] || root).c.push(row)
+  }
+
   head(res, 'Навигация')
 
   head.tail(res)
