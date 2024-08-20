cb7e307a40aa2f4c3634dbf71b45421dcf54517a
Stas Ukolov <ukoloff@gmail.com>
2024-08-20 12:00:06 +0500

    Simplify

--- src/sql2it.js
+++ src/sql2it.js
@@ -3,11 +3,7 @@
 //
 
 async function* feed(request) {
-  var done = false
-  var error = false
-  var reason
-  var consumers = []
-  var rows = []
+  let cb, data
 
   request
     .on('row', row)
@@ -15,34 +11,23 @@ async function* feed(request) {
     .on('error', croak)
     .stream = true
 
-  while (!done) {
-    if (rows.length) {
-      yield rows.shift()
-      continue
-    }
-    if (error) {
-      yield new Promise.reject(reason)
-      break
-    }
-    yield new Promise(...args => consumers.push(args))
+  while (true) {
+    var status = await new Promise(resolve => cb = resolve)
+    if (!status) break
+    yield status > 0 ? data : Promise.reject(data)
   }
 
   function row(row) {
-    if (consumers.length) {
-      consumers.shift()[0](row)
-      return
-    }
-    rows.push(row)
+    data = row
+    cb(1)
   }
 
   function finish() {
-    done = true
-    for (let z of consumers) z[0]()
+    cb(0)
   }
 
   function croak(err) {
-    error = true
-    reason = err
-    for (let z of consumers) z[1](err)
+    data = err
+    cb(-1)
   }
 }
