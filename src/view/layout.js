d951f8e630c19bd216acaabaa60d020616150834
Stas Ukolov <ukoloff@gmail.com>
2024-08-23 15:27:00 +0500

    Change theme

--- src/view/layout.js
+++ src/view/layout.js
@@ -9,7 +9,7 @@ async function layout(res, title, content) {
 <!DOCTYPE html><html><head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>${title}</title>
-<link href="https://cdn.jsdelivr.net/npm/bootswatch@5.3.3/dist/journal/bootstrap.min.css" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/bootswatch@5.3.3/dist/litera/bootstrap.min.css" rel="stylesheet">
 <link href="${res.$base}assets/kb.css" rel="stylesheet">
 <script src="${res.$base}assets/kb.js"></script>
 </head>
