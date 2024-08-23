ff376f959a271ae94dac90c4ab61533441fd50c2
Stas Ukolov <ukoloff@gmail.com>
2024-08-23 10:56:15 +0500

    Allow set number of workers

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-const cluster = require('node:cluster');
-const numCPUs = require('node:os').availableParallelism();
-const process = require('node:process');
+const cluster = require('node:cluster')
+const os = require('node:os')
+const process = require('node:process')
+require('dotenv/config')
 
 if (cluster.isPrimary) {
-  console.log("MAIN:", process.pid)
+  const numCPUs = parseInt(process.env.NUM) || os.availableParallelism()
+  console.log(`MAIN<${numCPUs}>: ${process.pid}`)
   for (let i = 0; i < numCPUs; i++) {
     cluster.fork();
   }
