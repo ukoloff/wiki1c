8fe4707a720b8edd07e6ef7691c2fd4ff1cdacc7
Stas Ukolov <ukoloff@gmail.com>
2024-08-23 14:24:22 +0500

    Calculate hash of tree

--- src/api/tree.js
+++ src/api/tree.js
@@ -1,7 +1,11 @@
+const crypto = require('node:crypto')
 const root = require('../model/tree')
 
 module.exports = tree
 
-async function tree(res) {
-  return root()
+async function tree(data) {
+  let z = await root()
+  let hash = crypto.createHash('sha256')
+  z.hash = hash.update(JSON.stringify(z.c)).digest('hex').slice(0, 12)
+  return z
 }
