ed74a0b5d14c88b384519a2c270f43ac0a16f235
Stas Ukolov <ukoloff@gmail.com>
2024-08-27 11:19:26 +0500

    Process request

--- src/view/theme.js
+++ src/view/theme.js
@@ -1,6 +1,7 @@
 //
 // Show themes to select
 //
+const qs = require('node:querystring')
 const html = require('./h')
 const layout = require('./layout')
 const bcz = require('./breadcz')
@@ -9,6 +10,8 @@ const themes = require('../model/themes')
 module.exports = theme
 
 async function theme($) {
+  if ($.req.method == 'POST')
+    return post($)
   $.content = list
   layout($)
 }
@@ -29,8 +32,9 @@ async function list($) {
       <label class="form-check-label"><input class="form-check-input" type="radio" name="theme" value="${theme}" required>
       ${html(theme.replace(/\w/, c => c.toUpperCase()))}
       </label>
+      <a href="${tz.url}${theme}" target="bootswatch"><i class="fa fa-eye"></i></a>
       `
-      .trim())
+      .trimStart())
   }
   res.write(`
     <hr>
@@ -43,4 +47,27 @@ async function list($) {
     </button>
     </div>
     `)
+}
+
+async function post($) {
+  let { theme, dark } = qs.decode(await read($.req))
+  let thz = await themes()
+  if (!thz.names.includes(theme))
+    theme = 'litera'
+  let cookie = theme
+  if (dark)
+    cookie += '!'
+  $.res.setHeader('Set-Cookie', `theme=${qs.escape(cookie)}; HttpOnly; Path=${$.base}`)
+  $.res.writeHead(301, { Location: '/' })
+    .end()
+}
+
+function read(stream) {
+  return new Promise((resolve, reject) => {
+    var body = ''
+    stream
+      .on('data', chunk => body += chunk)
+      .on('end', _ => resolve(body))
+      .on('error', reject)
+  })
 }
