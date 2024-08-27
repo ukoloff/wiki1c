df9019788279b66d79228dc8726354792b44a78c
Stas Ukolov <ukoloff@gmail.com>
2024-08-27 16:34:06 +0500

    Fetch first cookie

--- src/util/cookies.js
+++ src/util/cookies.js
@@ -7,6 +7,7 @@ function cookies($) {
   return Object.fromEntries(
     ($.req.headers.cookie || '')
       .split(/\s*;\s*/)
+      .toReversed()
       .map(x =>
         x
           .split(/\s*=\s*/, 2)
